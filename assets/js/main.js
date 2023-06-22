31773a85755713cc936ac64f62f5b58b4f74882d
Nuri Demir <nuridemir34@hotmail.com>
2023-06-22 18:42:24 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+(function () {
+    const loaderContainer = document.querySelector('.loading');
+    window.addEventListener('load', () => {
+        loaderContainer.style.display = 'none';
+    });
+})();
